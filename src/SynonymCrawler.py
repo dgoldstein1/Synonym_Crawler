b334096826c363b9ccb21a891dcc1e89373edccd
Dave <Dgoldstein01@gmail.com>
2016-11-09 17:36:03 -0500

    working GUI with model

---
+++ src/SynonymCrawler.py
@@ -0,0 +1,211 @@
+"""
+Created by david on 10.24.2016
+"""
+
+import sys
+import time
+import random
+from bs4 import BeautifulSoup
+from urllib import urlopen
+from treelib import Tree, Node
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class SynonymCrawler():
+	def __init__(self,word,leafWidth=2,treeHeight=5):
+
+		#init instance variables
+		self.currWord = word
+		self.leafWidth = leafWidth
+		self.treeHeight = treeHeight
+
+		#init model
+		self.targetData = self.crawl(self.currWord) #[0] = targetWord, [1] = targetTree
+		self.tree = self.__genChildren(self.currWord,1) #generate root and its children
+		self.currDefinition = self.scrapeDefinition(self.currWord)
+
+	def step(self,chosen_word):
+		""" 
+		called by GUI
+		processes chosen word by user
+		returns true if target word
+		else returns [currWord,definition,synonyms] to be displayed
+		"""
+		if chosen_word not in self.tree:
+			print chosen_word, " not in Tree"
+			self.tree.show()
+			return None
+
+
+		if len(self.tree.children(chosen_word))==0:
+			self.tree = self.__genChildren(chosen_word,1,self.tree,parent=self.currWord)
+
+		self.currWord = chosen_word
+		self.currDefinition = self.scrapeDefinition(chosen_word)
+		synonyms = []
+		for node in self.tree.children(chosen_word):
+			synonyms.append(node.tag)
+
+		self.tree.show()
+
+		print "displaying: ", [self.currWord,self.currDefinition,synonyms]
+
+		return [self.currWord,self.currDefinition,synonyms]
+
+	def scrapeDefinition(self,word):
+		"""parses definition of given word from online
+		@params:
+		    word 			- Required  : starting point in thesaurus.com (string)
+	    """
+		url = 'http://www.thesaurus.com/browse/' + word + '?s=t'
+		html = urlopen(url).read()
+		soup = BeautifulSoup(html,"html.parser")
+
+		#get definition
+		definitionBlock = soup.find('div',{"class":"mask"})
+		if definitionBlock == None:
+			print "Could not find synonym block in parsing for URL: " + url
+			return
+
+		definition = ""
+		for block in definitionBlock.findAll('strong',{"class":"ttl"}):
+			definition += block.renderContents() + ", "
+
+		return definition
+
+	def scrapeSynonyms(self,word,number,tree=None):
+		"""
+		scrapes synonyms of word from thesaurus.com
+		@params:
+		    word 			- Required  : starting point in thesaurus.com (string)
+		    number 		    - Required  : number of synonyms to scrape (int)
+		    tree 		   	- Optional  : specify tree to avoid repeating synonyms in tree  (treelib.Tree)
+	    """
+		url = 'http://www.thesaurus.com/browse/' + word + '?s=t'
+		html = urlopen(url).read()
+		soup = BeautifulSoup(html,"html.parser")
+
+		#get synonym list
+		synblock = soup.find('div',{"class":"relevancy-list"}) #synonym box
+		i = 0
+		synonyms = []
+		for syn in synblock.findAll('a'):
+			if i >= number:
+				break
+			word = syn.span.renderContents()			
+			if tree!=None and not tree.contains(word):				
+				synonyms.append(word)				
+				i = i + 1
+
+		return synonyms
+
+	def generateFullTree(self,startingWord,treeHeight=3,leafWidth=2,printOutput=True):
+		"""
+		generates the entire tree (with answer)
+		parses thesaurus entries up to treeHeight. Root node = height 0 
+		@params:
+	        startingWord   	- Required  : starting point in thesaurus.com (string)
+	        treeHeight      - Optional  : depth of crawl (int)
+	        leafWidth    	- Optional  : children considered at each iteration (int)
+	        printOutput     - Optional  : prints % progress after each iteration (bool)    
+		"""
+		totalNodes = (leafWidth ** (treeHeight + 1)) -  1
+		if printOutput:
+			printProgress(0, totalNodes,)
+		tree=self.__genChildren(startingWord,treeHeight,printOutput=printOutput)
+		if printOutput:
+			printProgress(totalNodes, totalNodes)
+		return tree
+
+	def __genChildren(self,currWord,nIterations,tree=Tree(),printOutput=True,currHeight=0,parent=None,nodesCreated=0):
+		"""
+		recusrively parses to generate tree of specified height and width
+		"""
+		#stop condition if reached spec. height
+		if currHeight > nIterations - 1:
+			return
+
+		#create root
+		if parent==None:
+			tree.create_node(currWord,currWord)
+
+		synonyms = self.scrapeSynonyms(currWord,self.leafWidth,tree)
+		for synonym in synonyms:
+			tree.create_node(synonym,synonym,parent = currWord)
+			nodesCreated = nodesCreated + 1
+			if printOutput:
+				printProgress(nodesCreated, (self.leafWidth ** (nIterations)),prefix="Generating Children")
+			
+
+		#recurse through children
+		for synonym in synonyms:
+			self.__genChildren(synonym,nIterations,tree,currHeight + 1,currWord,nodesCreated)
+
+		return tree
+
+	def crawl(self,startingWord,tree=Tree(),currdepth=0):
+		"""
+		crawls until n depth and returns random synonym
+	    @params:
+	    	self			- Required	: instance of Synonyms Crawler
+	        startingWord   	- Required  : starting point in thesaurus.com (string)
+	        tree       		- Optional  : specified tree to add on to (treelib.Tree)
+	        currdepth      	- Optional  : start on specified depth (int)
+		"""
+		if currdepth==0:
+			tree.create_node(startingWord,startingWord)
+
+		if currdepth == self.treeHeight:
+			return [startingWord,tree]
+
+
+		try:
+			nextWord = self.scrapeSynonyms(startingWord,self.leafWidth,tree)[random.randint(0,self.leafWidth-1)]
+		except:
+			nextWord = self.scrapeSynonyms(startingWord,self.leafWidth,tree)[0]					
+		tree.create_node(nextWord,nextWord,parent=startingWord)
+
+		printProgress(currdepth+1,self.treeHeight,prefix="Crawling")
+		return self.crawl(nextWord,tree,currdepth + 1)
+
+		#end class
+
+def printProgress (iteration, total, prefix = 'generating tree', suffix = '', decimals = 1, barLength = 40):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barLength   - Optional  : character length of bar (Int)
+    """
+    formatStr       = "{0:." + str(decimals) + "f}"
+    percents        = formatStr.format(100 * (iteration / float(total)))
+    filledLength    = int(round(barLength * iteration / float(total)))
+    bar             = 'X' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+	start_time = time.time()
+	craler = SynonymCrawler("mean")
+	print("--- executed in %s seconds ---" % (time.time() - start_time))
+"""
+
+	generateTree(word,leafWidth=leafWidth,treeHeight=treeHeight).show(key=lambda x: x.tag, reverse=True, line_type='ascii-em')	
+	crawl(word,leafWidth=leafWidth,treeHeight=treeHeight)[1].show(key=lambda x: x.tag, reverse=True, line_type='ascii-em')
+
+
+	"""
+
+
+
+ 
+	
+
--- src/gui.py
+++ src/gui.py
@@ -1,31 +1,36 @@
 import gi
+import SynonymCrawler
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
 class SynonymWindow(Gtk.Window):
 
-    def __init__(self):
-        Gtk.Window.__init__(self, title="HeaderBar Demo")
-        self.set_border_width(15)
-        self.set_default_size(300,300)
+    def __init__(self,startingWord,crawler=None):
+        Gtk.Window.__init__(self, title=startingWord)
+
+        #init instance vairables
+        self.crawler = SynonymCrawler.SynonymCrawler(startingWord,leafWidth=5)
+        self.number_of_synonyms = self.crawler.leafWidth
+
+        #init window
+        self.set_border_width(5)
+        self.set_default_size(450,400)
         self.__createHeader()
         self.__createBody()
+    
 
     def __createHeader(self):
         self.hb = Gtk.HeaderBar()
         self.hb.set_show_close_button(True)
-        self.hb.props.title = "Header"
-        self.hb.set_subtitle("subtitle")
+        self.hb.props.title = self.crawler.currWord
+        self.hb.set_subtitle(self.crawler.currDefinition)
         self.set_titlebar(self.hb)
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(box.get_style_context(), "linked")
 
         button = Gtk.Button()
         button.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
-        box.add(button)
-
-        button = Gtk.Button()
-        button.add(Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.NONE))
+        button.connect("clicked", self.on_back_pressed)
         box.add(button)
 
         self.hb.pack_start(box)
@@ -33,11 +38,9 @@ class SynonymWindow(Gtk.Window):
     def __createBody(self):        
         listbox = Gtk.ListBox()
         listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-
         self.__currSynonymBoxes = []
-        listbox.add(self.__synonymBox("synoynm 1"))
-        listbox.add(self.__synonymBox("synonym 2"))
-        listbox.add(self.__synonymBox("synonym 3")) 
+        for node in self.crawler.tree.children(self.crawler.currWord):
+            listbox.add(self.__synonymBox(node.tag))        
         self.add(listbox)
 
     def __synonymBox(self,word,textSize=15):
@@ -55,18 +58,39 @@ class SynonymWindow(Gtk.Window):
         button.connect("clicked", self.on_synonym_clicked)
         hbox.pack_start(button, True, True, 0)
 
-        self.__currSynonymBoxes.append(row)
+        self.__currSynonymBoxes.append(button)
         return row
 
-    def on_synonym_clicked(self,widget):
-        self.hb.props.title = widget.__value
-        self.hb.set_subtitle(widget.__value)
+
+    def on_synonym_clicked(self,widget=None,word=None):
+        if word == None:
+            word = widget.__value
+
+        data = self.crawler.step(word)
+
+        self.hb.props.title = word
+        self.hb.set_subtitle(data[1])
+
+        for i in range(0,len(self.__currSynonymBoxes)):
+            self.__currSynonymBoxes[i].set_label(data[2][i])
+            self.__currSynonymBoxes[i].__value = data[2][i]
+
+
+    def on_back_pressed(self,widget):
+        try:
+            previousWord = self.crawler.tree.parent(self.crawler.currWord).tag
+        except(AttributeError): #at root
+            return
+        self.on_synonym_clicked(word=previousWord)
+        
+
+
 
 
 
 
 if __name__ == "__main__":
-    win = SynonymWindow()
+    win = SynonymWindow("fiber")
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
     Gtk.main()
